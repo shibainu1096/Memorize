0ea3e62cdaae83fcfbdc45fad12769edf196fbbf
Xuexiang Xu <xuexu@microsoft.com>
2022-12-25 19:28:35 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Xuexiang Xu on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
